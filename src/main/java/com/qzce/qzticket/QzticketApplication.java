c5d3db6b4c4e7b2f7f1db307067ca0bb57cea813
qzce <speed9742@naver.com>
2022-12-22 22:29:03 +0900

    Initial commit

---
+++ src/main/java/com/qzce/qzticket/QzticketApplication.java
@@ -0,0 +1,13 @@
+package com.qzce.qzticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QzticketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QzticketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qzce/qzticket/QzticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qzce.qzticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QzticketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
